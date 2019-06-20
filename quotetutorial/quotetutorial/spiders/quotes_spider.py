7f36d754a27e49a553eb9e7e35ee34ef19f94207
simaung <ddhr00@gmail.com>
2019-06-20 15:00:19 +0700

    logging in with scrapy formrequest

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,15 +1,24 @@
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
-    page_number = 2
     start_urls = [
-            'http://quotes.toscrape.com/page/1'
+            'http://quotes.toscrape.com/login'
             ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token'    : token,
+            'username'      : 'dede@email.com',
+            'password'      : 'tes'
+        }, callback = self.start_scraping)
 
+    def start_scraping(self, response):
+        open_in_browser(response)
         items = QuotetutorialItem()
 
         all_div_quotes = response.css('div.quote')
@@ -24,9 +33,3 @@ class QuoteSpider(scrapy.Spider):
             items['tag'] = tag
 
             yield items
-
-        next_page = 'http://quotes.toscrape.com/page/'+ str(QuoteSpider.page_number) + '/'
-
-        if QuoteSpider.page_number < 11:
-            QuoteSpider.page_number += 1
-            yield response.follow(next_page, callback= self.parse)
